43649ac5bc3a8183652f00a3a776b3e122a1a898
moatez <tazt7475@gmail.com>
2021-06-03 17:00:59 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+/** @format */
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { InputGroup, FormControl, Form } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+        <FormControl
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Recipient's username"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
+      </InputGroup>
+
+      <Form.Label htmlFor="basic-url">Your vanity URL</Form.Label>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon3">
+          https://example.com/users/
+        </InputGroup.Text>
+        <FormControl id="basic-url" aria-describedby="basic-addon3" />
+      </InputGroup>
+
+      <InputGroup className="mb-3">
+        <InputGroup.Text>$</InputGroup.Text>
+        <FormControl aria-label="Amount (to the nearest dollar)" />
+        <InputGroup.Text>.00</InputGroup.Text>
+      </InputGroup>
+
+      <InputGroup>
+        <InputGroup.Text>With textarea</InputGroup.Text>
+        <FormControl as="textarea" aria-label="With textarea" />
+      </InputGroup>
+    </div>
+  );
+}
+
+export default App;
